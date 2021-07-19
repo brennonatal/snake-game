65cdc865c0f8b37bee2fe139c039c749d0426355
Brenno Natal <natalbrenno@gmail.com>
2021-07-19 19:31:48 -0300

    Adding some comments and rules

--- main.py
+++ main.py
@@ -14,12 +14,16 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 green = (53, 192, 74)
 red = (250, 0, 0)
+
 # Setting screen
 screen = pygame.display.set_mode(size)
+
 # Defining fonts to use
 playFont = pygame.font.Font('assets/fonts/SigmarOne-Regular.ttf', 30)
 titleFont = pygame.font.Font('assets/fonts/OtomanopeeOne-Regular.ttf', 80)
 regularFont = pygame.font.Font('assets/fonts/OpenSans-Regular.ttf', 25)
+smallFont = pygame.font.Font('assets/fonts/OpenSans-Regular.ttf', 20)
+
 # Display background menu
 bg = pygame.image.load('assets/images/bg-menu.jpeg')
 bg = pygame.transform.scale(bg, size)
@@ -27,14 +31,18 @@ bg = pygame.transform.scale(bg, size)
 # Initialized as True to create a game loop
 game_over = True
 score = None
+
 # Initializing snake head
 snake = Snake()
+
 # To keep track of the moves
 x_move = 0
 y_move = 0
-# Food coordinates
+
+# Food properties
 food_x = round(random.randrange(0, width - snake.width) / 10) * 10
 food_y = round(random.randrange(0, height - snake.height) / 10) * 10
+food_ratio = 4
 
 clock = pygame.time.Clock()
 
@@ -43,24 +51,38 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        if event.type == pygame.KEYDOWN and not game_over:
-            if event.key == pygame.K_LEFT:
-                x_move = -5
-                y_move = 0
-            elif event.key == pygame.K_RIGHT:
-                x_move = 5
-                y_move = 0
-            elif event.key == pygame.K_UP:
-                y_move = -5
-                x_move = 0
-            elif event.key == pygame.K_DOWN:
-                y_move = 5
-                x_move = 0
+        if event.type == pygame.KEYDOWN:
+            # Key options when not playing
+            if game_over:
+                # Play game if ENTER is pressed
+                if event.key == pygame.K_RETURN:
+                    time.sleep(0.2)
+                    score = 0
+                    game_over = False
+                # Quit game if ESC is pressed
+                if event.key == pygame.K_ESCAPE:
+                    sys.exit()
+            # While playing
+            else:
+                # Snake controllers
+                if event.key == pygame.K_LEFT and x_move == 0:
+                    x_move = -5
+                    y_move = 0
+                elif event.key == pygame.K_RIGHT and x_move == 0:
+                    x_move = 5
+                    y_move = 0
+                elif event.key == pygame.K_UP and y_move == 0:
+                    y_move = -5
+                    x_move = 0
+                elif event.key == pygame.K_DOWN and y_move == 0:
+                    y_move = 5
+                    x_move = 0
 
     screen.blit(bg, (0, 0))
 
-    # To display the initial menu
+    # To display the initial menu or game over
     if game_over:
+        # When game is over
         if score != None:
             screen.fill(red)
 
@@ -93,6 +115,7 @@ while True:
                     time.sleep(0.2)
                     score = 0
                     game_over = False
+        # Initial menu
         else:
             # Draw title
             title = titleFont.render('Snake game', True, black)
@@ -121,26 +144,29 @@ while True:
         screen.fill(black)
 
         # Draw score
-        score_text = regularFont.render(f'Score: {score}', True, white)
+        score_text = smallFont.render(f'Score: {score}', True, white)
         scoreRect = score_text.get_rect()
         scoreRect.center = (50, 20)
         screen.blit(score_text, scoreRect)
 
+        # Constantly move the snake using last movement
         snake.x += x_move
         snake.y += y_move
+
         # Draw snake
         pygame.draw.rect(
             screen, green, [snake.x, snake.y, snake.height, snake.width])
+
         # Draw food
         pygame.draw.circle(
-            screen, red, (food_x, food_y), 4)
+            screen, red, (food_x, food_y), food_ratio)
         pygame.display.update()
 
         # Checking if the snake hit the food
         if (snake.x < food_x + 8 and
-            snake.x + snake.width + 4 > food_x and
+            snake.x + snake.width + food_ratio > food_x and
             snake.y < food_y + 8 and
-                snake.y + snake.height + 4 > food_y):
+                snake.y + snake.height + food_ratio > food_y):
             # Increasing score
             score += 1
             # Food new position
